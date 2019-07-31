8b7be4311456a3a52e3848d25ec663cb6b037191
Dylan <dgstrau@gmail.com>
2019-07-31 15:12:23 -0400

    adding 'worlds hardest' example

--- solver.py
+++ solver.py
@@ -206,3 +206,8 @@ myboard.print_board()
 
 mysolver = Solve(myboard)
 mysolver.solution.print_board()
+
+
+# '8..........36......7..9.2...5...7.......457.....1...3...1....68..85...1..9....4..'
+# the preceeding string sample input string for the 'world's hardest' sodoku puzzle presented here:
+# https://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html
